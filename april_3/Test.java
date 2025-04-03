61149175d8c44cf2c285a56fef094cdf7c5e9c2f
Youra <a@a.c>
2025-04-03 14:23:57 +0200

    3_april

---
+++ april_3/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        int year = 2024;
+        System.out.printf("Year %d isLeap: %b\n", year, Calendars.isLeap(year));
+    }
+}
